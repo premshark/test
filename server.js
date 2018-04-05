4fd3ad9c0501efbf30736db4f76437f10278ecee
jeyakumar8181 <37777450+jeyakumar8181@users.noreply.github.com>
2018-04-05 17:09:39 +0530

    kumar update

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var http = require('http');
 http.createServer(function(req, res){
 
 res.writeHead(200, {'content-type':'text/plain'});
-res.end('Hello jai!');
+res.end('Hello jai kumar!');
 }).listen(9001);
 
 console.log('Server is running on a port over 9001 !!');
