5718abb884392b0f30a1eeff62882d2ed3206134
jeyakumar8181 <37777450+jeyakumar8181@users.noreply.github.com>
2018-04-18 16:05:26 +0530

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var http = require('http');
 http.createServer(function(req, res){
 
 res.writeHead(200, {'content-type':'text/plain'});
-res.end('Welcome to ECS');
+res.end('Welcome prem');
 }).listen(9001);
 
 console.log('Server is running on a port over 9001 !!');
